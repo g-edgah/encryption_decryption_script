484987a3cab25332b0686012218fcc6672ea3138
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-11-20 04:03:42 +0400

    encryption

--- lib/encrypt.sh
+++ lib/encrypt.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-encrypt_file() {
+if [ -f ".env" ]; then
+     set -a  # Automatically export all variables
+     source .env
+     set +a  # Stop auto-exporting
+fi
 
+KEY="$KEY"
+
+encrypt_file() {
+    file=$1
+    echo "encrypting $file"
+    if [[ $KEY ]]; then
+        gpg --batch --yes --passphrase "$KEY" -c --cipher-algo AES256 $file
+        rm -f $file
+    elif [[ ! $KEY ]]; then
+        echo -e "no predefined key provided"
+        gpg --batch --yes -c --cipher-algo AES256 $file
+    fi
 }
--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ if [ -f ".env" ]; then
 fi
 
 FOLDER="$FOLDER"
-KEY="$KEY"
+
 
 source ./lib/decrypt.sh
 source ./lib/encrypt.sh
@@ -22,7 +22,9 @@ main() {
         echo "  3. exit"
         read -p "select an option [1/2/3]: " -r
         
+
         if [[ $REPLY == 1 ]]; then
+
            while true; do
                 echo "encrypting"
                 echo "encrypt?"
@@ -32,8 +34,61 @@ main() {
                 read -p "select an option [1/2/3]: " -r
 
                 if [[ $REPLY == 1 ]]; then
-                elif [[ $REPLY == 2]]; then
-                elif [[ $REPLY == 3]]; then
+
+                    if [[ -d $FOLDER ]]; then
+                        read -p "encrypt folder $FOLDER [Y/n]?: " folder
+                        if [[ $folder =~ ^[Yy]$ ]] || [[ -z $folder ]]; then
+                            for file in "${folder[@]}"; do
+                                encrypt_file "$file"
+                                echo "$file"
+                            done
+                        elif [[ $confirm =~ ^[Nn] ]]; then
+                            echo  -e "encryption cancelled"
+                        else
+                            echo -e "invalid option"
+                        fi
+                    elif [[ ! -d $FOLDER ]]; then
+                        read -p "no folder specified. enter path of folder containing files: " input_folder
+                        if 
+
+                    fi
+
+                elif [[ $REPLY == 2 ]]; then
+                    files=()
+                    i=1
+                    while true; do
+                        read -p "enter relative or full path of file $i: " filepath
+
+                        if [[ -z "$filepath" ]]; then
+                            echo -e "\n no input provided \n"
+                            continue
+                        elif [[ "$filepath" == "done" ]]; then
+                            echo "done collecting files to encrypt"
+                            break
+                        elif [[ -f "$filepath" ]]; then
+                            files+=("$filepath")
+                            ((i++))
+                        elif [[ ! -f "$filepath" ]]; then
+                            echo " file not found: $filepath"
+                        fi
+                    done
+
+                    read -p "encrypt files [Y/n]: " confirm
+
+                    if [[ $confirm =~ ^[Yy]$ ]] || [[ -z $confirm ]]; then
+                        for file in "${files[@]}"; do
+                            encrypt_file "$file"
+                            echo "$file"
+                        done
+                    elif [[ $confirm =~ ^[Nn] ]]; then
+                        echo  -e "encryption cancelled"
+                    else
+                        echo -e "invalid option"
+                    fi
+
+
+                elif [[ $REPLY == 3 ]]; then
+                    console "something"
                 elif [[ -z "$REPLY" ]]; then
                     echo -e "\n no option selected. please pick an option \n"
                 else
@@ -41,6 +96,8 @@ main() {
                 fi
             done
             break
+
+
         elif [[ $REPLY == 2 ]]; then
             while true; do
                 echo "decrypting"
@@ -51,8 +108,11 @@ main() {
                 read -p "select an option [1/2/3]: " -r
 
                 if [[ $REPLY == 1 ]]; then
-                elif [[ $REPLY == 2]]; then
-                elif [[ $REPLY == 3]]; then
+                    echo "1"
+                elif [[ $REPLY == 2 ]]; then
+                    echo "2"
+                elif [[ $REPLY == 3 ]]; then
+                    echo "3"
                 elif [[ -z "$REPLY" ]]; then
                     echo -e "\n no option selected. please pick an option \n"
                 else
@@ -60,11 +120,17 @@ main() {
                 fi
             done
             break
+
+            
         elif [[ $REPLY == 3 ]]; then
             echo "exiting"
             break
+
+
         elif [[ -z "$REPLY" ]]; then
             echo -e "\n no option selected. please pick an option \n"
+
+
         else
             echo -e "\n invalid option. please pick an option from those provided \n"
         fi
